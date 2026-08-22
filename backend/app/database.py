cbdb2450da599ea163e299a7f395baf51a01e652
Ayush Shaw <ayushaks999@gmail.com>
2026-08-22 17:55:15 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,13 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from .config import settings
-
-
 class Base(DeclarativeBase):
     pass
 
-
+# Then The responsible for connecTing your backend To The daTabase and creaTing DB sessions for API requesTs.
 connect_args = {"check_same_thread": False} if settings.database_url.startswith("sqlite") else {}
 engine = create_engine(settings.database_url, pool_pre_ping=True, connect_args=connect_args)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
 
+SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
